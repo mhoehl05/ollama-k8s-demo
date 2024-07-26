ab56fcd46a127ba3b3c24bb5a5043c1ba783120d
Höhl <MHOEHL@controlware.de>
2024-07-26 09:53:14 +0200

    removed out put for kubeconfig due to remote exec

--- output.tf
+++ output.tf
@@ -2,10 +2,4 @@ output "nodes_url" {
   value = ovh_cloud_project_kube.mycluster.nodes_url
 }
 
-resource "local_sensitive_file" "kubeconfig" {
-  sensitive_content = ovh_cloud_project_kube.mycluster.kubeconfig
-  filename             = "${path.module}/files/kubeconfig.yaml"
-  file_permission      = 0777
-  directory_permission = 0777
-}
 
