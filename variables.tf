6dcdb76cac72a07c6c27cf9eb62bd5a86e8f11e5
Höhl <MHOEHL@controlware.de>
2024-07-26 10:57:41 +0200

    created variable for node size

--- variables.tf
+++ variables.tf
@@ -9,3 +9,9 @@ variable "ovh_application_secret" {
 variable "ovh_consumer_key" {
   type = string
 }
+
+variable "node_size" {
+  type        = string
+  default     = "l4-90"
+  description = "flavor name that defines the size of the work node (needs to be gpu enabled and default to cheapest gpu flavor)"
+}
