5c5344fb08402b163c549ff548e8815e47182fd2
Höhl <MHOEHL@controlware.de>
2024-07-26 09:12:26 +0200

    removed helm config and changed kubeconfig output

--- output.tf
+++ output.tf
@@ -1,9 +1,11 @@
-output "kubeconfig_file" {
-  content   = ovh_cloud_project_kube.mycluster.kubeconfig
-  filename  = "kubeconfig.yaml"
-  sensitive = true
-}
-
 output "nodes_url" {
   value = ovh_cloud_project_kube.mycluster.nodes_url
 }
+
+resource "local_sensitive_file" "kubeconfig" {
+  sensitive_content = ovh_cloud_project_kube.mycluster.kubeconfig
+  filename             = "${path.module}/files/kubeconfig.yaml"
+  file_permission      = 0777
+  directory_permission = 0777
+}
+
--- provider.tf
+++ provider.tf
@@ -4,13 +4,3 @@ provider "ovh" {
   application_secret = var.ovh_application_secret
   consumer_key       = var.ovh_consumer_key
 }
-
-provider "helm" {
-  kubernetes {
-    config_path = "./kubeconfig.yaml"
-  }
-
-  depends_on = [
-    output.kubeconfig_file
-  ]
-}
