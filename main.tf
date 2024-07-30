c331fc74ef1a1b70fbc1111cbf819f22a789d270
Höhl <MHOEHL@controlware.de>
2024-07-30 12:54:31 +0200

    created variable and values for ollama deployment gpu limits

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+locals {
+  gpu_count = 2
+}
+
+
 resource "ovh_cloud_project_kube" "mycluster" {
   service_name = "f17cf6a8b8b14b08a4958b53dc8105a5"
   name         = "my_kube_cluster"
@@ -28,6 +33,11 @@ resource "helm_release" "gpu_operator" {
   ]
 }
 
+resource "local_file" "ollama_values" {
+  content  = templatefile("${path.module}/values.tftpl", { gpu_count = local.gpu_count })
+  filename = "${path.module}/helmchart/ollama-demo-helmchart/values.yaml"
+}
+
 resource "helm_release" "ollama_demo" {
   name             = "ollama-demo-helmchart"
   chart            = "./helmchart/ollama-demo-helmchart"
@@ -35,7 +45,8 @@ resource "helm_release" "ollama_demo" {
   create_namespace = true
 
   depends_on = [
-    ovh_cloud_project_kube_nodepool.node_pool
+    ovh_cloud_project_kube_nodepool.node_pool,
+    local_file.ollama_values
   ]
 }
 
