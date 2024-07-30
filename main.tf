f3090b217ba712010a6a3e5c8b7b664833011070
Moritz <moritz.schuermann@controlware.de>
2024-07-30 10:25:19 +0200

    png added

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "ovh_cloud_project_kube_nodepool" "node_pool" {
   service_name   = "f17cf6a8b8b14b08a4958b53dc8105a5"
   kube_id        = ovh_cloud_project_kube.mycluster.id
   name           = "my-pool-1" //Warning: "_" char is not allowed!
-  flavor_name    = var.node_size
+  flavor_name    = var.flavor_name
   desired_nodes  = 1
   max_nodes      = 1
   min_nodes      = 1
--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "ovh_consumer_key" {
 # Possible options: https://www.ovhcloud.com/en/public-cloud/prices/
 
 
-variable "node_size" {
+variable "flavor_name" {
   type        = string
   default     = "l4-90"
   description = "flavor name that defines the size of the work node (needs to be gpu enabled and default to cheapest gpu flavor)"
